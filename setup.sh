9f371f51b2acde4fd398fb20ae1721482f3a1f31
Armaan <armaankaurgill@gmail.com>
2022-01-03 21:47:58 +0530

    First cmmit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DATABASE_URL="postgres://pdwptvgutkzryv:9412ade8f0b38973cf7d7d1219e9673d588c89e0f92eac3a5a1ae81eaefa2a9f@ec2-54-236-156-167.compute-1.amazonaws.com:5432/d6hmdtl72hg1j"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
